5876853d2d8a2c2fa1676733358655a585f4b147
yeahg-dev <yeahg.dev@gmail.com>
2022-05-19 23:36:53 +0900

    projects: JuiceMaker-MVVM 프로젝트 생성

---
+++ JuiceMaker-MVVM/JuiceMaker-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JuiceMaker-MVVM
+//
+//  Created by 1 on 2022/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
