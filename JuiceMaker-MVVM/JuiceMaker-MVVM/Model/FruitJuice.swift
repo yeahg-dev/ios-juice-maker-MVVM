7daddc180d0eece91f737a3588837de404adb817
yeahg-dev <yeahg.dev@gmail.com>
2022-05-21 09:38:26 +0900

    🔨feat: FruitJuice 타입 정의

---
+++ JuiceMaker-MVVM/JuiceMaker-MVVM/Model/FruitJuice.swift
@@ -0,0 +1,101 @@
+//
+//  FruitJuice.swift
+//  JuiceMaker-MVVM
+//
+//  Created by 1 on 2022/05/20.
+//
+
+import Foundation
+
+protocol FruitJuice {
+    
+    var juice: String { get }
+    
+    var ingredients: [Fruit: Int] { get }
+    
+}
+
+struct StrawberryJuice: FruitJuice {
+    
+    var juice: String {
+        return "딸기 쥬스"
+    }
+    
+    var ingredients: [Fruit: Int] {
+        return [Fruit.strawberry: 7]
+    }
+    
+}
+
+struct PeachJuice: FruitJuice {
+    
+    var juice: String {
+        return "복숭아 쥬스"
+    }
+    
+    var ingredients: [Fruit: Int] {
+        return [Fruit.peach: 3]
+    }
+    
+}
+
+struct StrawberryPeachJuice: FruitJuice {
+    
+    var juice: String {
+        return "딸복 쥬스"
+    }
+    
+    var ingredients: [Fruit: Int] {
+        return [Fruit.strawberry: 5, Fruit.peach: 2]
+    }
+    
+}
+
+struct WatermelonJuice: FruitJuice {
+    
+    var juice: String {
+        return "수박 쥬스"
+    }
+    
+    var ingredients: [Fruit: Int] {
+        return [Fruit.watermelon: 1]
+    }
+    
+}
+
+struct PineappleJuice: FruitJuice {
+    
+    var juice: String {
+        return "파인애플 쥬스"
+    }
+    
+    var ingredients: [Fruit: Int] {
+        return [Fruit.pineapple: 1]
+    }
+    
+}
+
+struct PineappleWatermelonJuice: FruitJuice {
+    
+    var juice: String {
+        return "수파 쥬스"
+    }
+    
+    var ingredients: [Fruit: Int] {
+        return [Fruit.watermelon: 1, Fruit.pineapple: 1]
+    }
+    
+}
+
+struct BananaJuice: FruitJuice {
+    
+    var juice: String {
+        return "바나나 쥬스"
+    }
+    
+    var ingredients: [Fruit: Int] {
+        return [Fruit.banana: 5]
+    }
+    
+}
+
