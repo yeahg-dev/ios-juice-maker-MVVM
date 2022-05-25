685c5c138ee468e9da6653a9f06350116e82b53e
yeahg-dev <yeahg.dev@gmail.com>
2022-05-25 17:54:56 +0900

    ✨refactor: 주스주문 이벤트를 IBAction에서 rxCocoa로 수정

--- JuiceMaker-MVVM/JuiceMaker-MVVM/View/JuiceMakerViewController.swift
+++ JuiceMaker-MVVM/JuiceMaker-MVVM/View/JuiceMakerViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
 class JuiceMakerViewController: UIViewController {
     
@@ -17,6 +18,14 @@ class JuiceMakerViewController: UIViewController {
     @IBOutlet weak var pineappleStockLabel: UILabel?
     @IBOutlet weak var watermelonStockLabel: UILabel?
     @IBOutlet weak var bananaStockLabel: UILabel?
+    
+    @IBOutlet weak var strawberryPeachJuiceButton: UIButton?
+    @IBOutlet weak var strawberryJuiceButton: UIButton?
+    @IBOutlet weak var peachJuiceButton: UIButton?
+    @IBOutlet weak var watermelonPineappleJuiceButton: UIButton?
+    @IBOutlet weak var pineappleJuiceButton: UIButton?
+    @IBOutlet weak var watermelonJuiceButton: UIButton?
+    @IBOutlet weak var bananaJuiceButton: UIButton?
     
     // MARK: - Property
     
@@ -34,11 +43,56 @@ class JuiceMakerViewController: UIViewController {
     // MARK: - bind UI
     
     private func bindUI() {
+        let juiceOrder = PublishSubject<FruitJuice>()
+        let input = JuiceMakerViewModel.Input(juiceOrder: juiceOrder)
+        let output = juiceMakerViewModel.transfrom(input: input)
+        
+        self.strawberryJuiceButton?.rx.tap
+            .bind {
+                juiceOrder.onNext(StrawberryJuice())
+            }
+            .disposed(by: disposeBag)
+        
+        self.strawberryPeachJuiceButton?.rx.tap
+            .bind {
+                juiceOrder.onNext(StrawberryPeachJuice())
+            }
+            .disposed(by: disposeBag)
+        
+        self.peachJuiceButton?.rx.tap
+            .bind {
+                juiceOrder.onNext(PeachJuice())
+            }
+            .disposed(by: disposeBag)
+        
+        self.watermelonPineappleJuiceButton?.rx.tap
+            .bind {
+                juiceOrder.onNext(PineappleWatermelonJuice())
+            }
+            .disposed(by: disposeBag)
+        
+        self.watermelonJuiceButton?.rx.tap
+            .bind {
+                juiceOrder.onNext(WatermelonJuice())
+            }
+            .disposed(by: disposeBag)
+        
+        self.pineappleJuiceButton?.rx.tap
+            .bind {
+                juiceOrder.onNext(PineappleJuice())
+            }
+            .disposed(by: disposeBag)
+        
+        self.bananaJuiceButton?.rx.tap
+            .bind {
+                juiceOrder.onNext(BananaJuice())
+            }
+            .disposed(by: disposeBag)
+        
         output.orderSuccess
             .withUnretained(self)
             .subscribe(onNext: { (owner, result) in
             self.loadFruitStock()
-            
         })
         .disposed(by: disposeBag)
         
@@ -76,35 +130,6 @@ class JuiceMakerViewController: UIViewController {
             })
             .disposed(by: disposeBag)
     }
-    
-    // MARK: - @IBAction
-    @IBAction func orderStrawberryPeachJuice(_ sender: UIButton) {
-        self.input.juiceOrder.onNext(StrawberryPeachJuice())
-    }
-    
-    @IBAction func orderStrawberryJuice(_ sender: UIButton) {
-        self.input.juiceOrder.onNext(StrawberryJuice())
-    }
-    
-    @IBAction func orderPeachJuice(_ sender: UIButton) {
-        self.input.juiceOrder.onNext(PeachJuice())
-    }
-    
-    @IBAction func orderWatermelonPineappleJuice(_ sender: UIButton) {
-        self.input.juiceOrder.onNext(PineappleWatermelonJuice())
-    }
-    
-    @IBAction func orderPineappleJuice(_ sender: UIButton) {
-        self.input.juiceOrder.onNext(PineappleJuice())
-    }
-    
-    @IBAction func orderWatermelonJuice(_ sender: UIButton) {
-        self.input.juiceOrder.onNext(WatermelonJuice())
-    }
-    
-    @IBAction func orderBananaJuice(_ sender: UIButton) {
-        self.input.juiceOrder.onNext(BananaJuice())
-    }
-    
+ 
 }
 
